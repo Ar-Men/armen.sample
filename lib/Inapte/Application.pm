ac7a5d415eeea206a62b03a55103cfb823a66056
Loïc TROCHET <losyme@protonmail.com>
2018-05-26 23:46:57 +0200

    Ajout du fichier 'Application.pm' obligatoire pour toute application

---
+++ lib/Inapte/Application.pm
@@ -0,0 +1,25 @@
+#######
+##                                                      __
+##   ___ _______ _  ___ ___        ___ ___ ___ _  ___  / /__
+##  / _ `/ __/  ' \/ -_) _ \  _   (_-</ _ `/  ' \/ _ \/ / -_)
+##  \_,_/_/ /_/_/_/\__/_//_/ (_) /___/\_,_/_/_/_/ .__/_/\__/
+##                                             /_/
+##
+####### Ecosystème basé sur les microservices ##################### (c) 2018 losyme ####### @(°_°)@
+
+package Inapte::Application;
+
+#md_# Inapte::Application
+#md_
+
+use Exclus::Exclus;
+
+#md_## Les méthodes
+#md_
+
+#md_### setup()
+#md_
+sub setup {}
+
+1;
+__END__
